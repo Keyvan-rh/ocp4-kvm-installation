3512d2532691288fd93da62679eda4882fde39b5
keyvan pishevar <kpisheva@redhat.com>
2020-07-02 08:19:58 -0500

    Create install-packages.sh

---
+++ host-prep/install-packages.sh
@@ -0,0 +1,7 @@
+yum -y update
+yum -y install tigervnc-server tigervnc tigervnc-server-module
+yum -y module install virt
+yum -y install virt-install virt-viewer
+yum -y install virt-manager
+yum -y install ansible
+yum -y install git
