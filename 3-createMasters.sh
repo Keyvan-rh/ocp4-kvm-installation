3f5aa27455de4f1a27e69b604f93e5e0430ef9fa
keyvan pishevar <kpisheva@redhat.com>
2020-10-14 13:00:49 -0500

    update ram and MAC address

--- 3-createMasters.sh
+++ 3-createMasters.sh
@@ -1,9 +1,9 @@
-MAC=(52:54:00:e7:9d:67 52:54:00:80:16:23 52:54:00:d5:1c:39)
+MAC=(00:50:56:11:22:11 00:50:56:11:22:22 00:50:56:11:22:33)
 for m in {0..2}
 do 
   i=master${m}
   #echo ${i}
-  virt-install --name="ocp4-${i}" --vcpus=4 --ram=10000 \
+  virt-install --name="ocp4-${i}" --vcpus=4 --ram=27000 \
   --disk path=/mnt/data/images/ocp4-${i}.qcow2,bus=virtio,size=120 \
   --os-variant rhel8.0 --network bridge=br0,model=virtio \
   --mac=${MAC[${m}]} \
