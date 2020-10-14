cdec5138543c6805ce154abc006ca77a2be54cfb
keyvan pishevar <kpisheva@redhat.com>
2020-10-14 12:54:20 -0500

    add boot sequence hd, network

--- 3-createMasters.sh
+++ 3-createMasters.sh
@@ -7,7 +7,7 @@ do
   --disk path=/mnt/data/images/ocp4-${i}.qcow2,bus=virtio,size=120 \
   --os-variant rhel8.0 --network bridge=br0,model=virtio \
   --mac=${MAC[${m}]} \
-  --boot menu=on --print-xml > ocp4-$i.xml
+  --boot hd,network,menu=on --print-xml > ocp4-$i.xml
   virsh define --file ocp4-$i.xml
   #echo ${MAC[${m}]}
 done
